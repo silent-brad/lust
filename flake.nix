ca05f4a5edaad1ed385e137a2ac8dd333c6b0d9f
bwhtdev <bradscottwhite@gmail.com>
2026-03-12 22:12:47 -0400

    added Nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Lust: Lua in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustfmt
+            clippy
+          ];
+        };
+      }
+    );
+}
